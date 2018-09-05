e1356643af202ed6cc0b7bc2f0a7d9fed72be31a
Jake Gough <super.jake@hotmail.co.uk>
2018-09-05 10:17:29 +0100

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ERSWebApp.Controllers
+namespace AspSpaCrudVue.Controllers
 {
     public class HomeController : Controller
     {
