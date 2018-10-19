bfc1349bf56fcd55ce984ea4618414bd5af5fb97
Jake Gough <super.jake@hotmail.co.uk>
2018-10-19 10:36:57 +0100

    Update app.ts
    added theme colour menu

--- ClientApp/components/app/app.ts
+++ ClientApp/components/app/app.ts
@@ -2,11 +2,51 @@ import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 
 @Component({
-    components: {
-        MenuComponent: require('../navmenu/navmenu.vue.html').default
-    }
+	components: {
+		MenuComponent: require('../navmenu/navmenu.vue.html').default
+	}
 })
 export default class AppComponent extends Vue {
 	drawer: boolean = false;
 	dark: boolean = false;
+	colours: string[] = ["red", "pink", "purple", "indigo", "blue", "teal", "green", "orange"
+	];
+
+	themeColour(colour: number) {
+		console.log(colour);
+		switch (colour) {
+			case 0:
+				this.$vuetify.theme.primary = "ED1C24";
+				this.$vuetify.theme.secondary = "f36368";
+				break;
+			case 1:
+				this.$vuetify.theme.primary = "E91E63";
+				this.$vuetify.theme.secondary = "FF80AB";
+				break;
+			case 2:
+				this.$vuetify.theme.primary = "9C27B0";
+				this.$vuetify.theme.secondary = "EA80FC";
+				break;
+			case 3:
+				this.$vuetify.theme.primary = "3F51B5";
+				this.$vuetify.theme.secondary = "8C9EFF";
+				break;
+			case 4:
+				this.$vuetify.theme.primary = "2196F3";
+				this.$vuetify.theme.secondary = "82B1FF";
+				break;
+			case 5:
+				this.$vuetify.theme.primary = "009688";
+				this.$vuetify.theme.secondary = "A7FFEB";
+				break;
+			case 6:
+				this.$vuetify.theme.primary = "4CAF50";
+				this.$vuetify.theme.secondary = "B9F6CA";
+				break;
+			case 7:
+				this.$vuetify.theme.primary = "FF9800";
+				this.$vuetify.theme.secondary = "FFD180";
+				break;
+		}
+	}
 }
