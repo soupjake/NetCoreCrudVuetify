1827f272cb01b67bde9d8e83072adf4b29f3aecf
Jake Gough <super.jake@hotmail.co.uk>
2018-10-29 10:48:22 +0000

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = (env) => {
                     }
                 ] },
                 { test: /\.css$/, use: isDevBuild ? [ 'style-loader', 'css-loader' ] : ExtractTextPlugin.extract({ use: 'css-loader?minimize' }) },
-                { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
+                { test: /\.(png|woff|woff2|eot|ttf|svg)$/, use: 'url-loader?limit=25000' }
             ]
         },
         output: {
