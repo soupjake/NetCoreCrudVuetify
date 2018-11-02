6b80808101715ac959ac61f357e058e95bde7c66
Jake Gough <super.jake@hotmail.co.uk>
2018-11-02 10:09:56 +0000

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
-namespace AspSpaCrudVue.Controllers
+namespace NetCoreCrudVuetify.Controllers
 {
     public class HomeController : Controller
     {
