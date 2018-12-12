cb491cb4703df880a330c9b5de72ff4f38d37158
Jake Gough <super.jake@hotmail.co.uk>
2018-12-12 10:16:27 +0000

    Update createemployee.ts
    improved validation for numbers and to not allow implicit anys

--- ClientApp/components/employee/createemployee.ts
+++ ClientApp/components/employee/createemployee.ts
@@ -9,9 +9,9 @@ export default class CreateEmployeeComponent extends Vue {
 	}
 
 	rules: object = {
-		required: value => !!value || 'Required',
-		number: value => /[0-9]/.test(value) || 'Value must be number e.g. "8" or "10"',
-		decimal: value => /^\d+(\.\d{1,2})?$/.test(value) || 'Value must be decimal e.g. "8.0" or "7.5"'
+		required: (value: string) => !!value || 'Required',
+		number: (value: string) => /^\d+(\d{1,2})?$/.test(value) || 'Value must be number e.g. "8" or "10"',
+		decimal: (value: string) => /^\d+(\.\d{1,2})?$/.test(value) || 'Value must be decimal e.g. "8.0" or "7.5"'
 	}
 	
 	failed: boolean = false;
