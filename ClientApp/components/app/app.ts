c09fabd966ca893e2c3e4d46974f2a1c0146c01c
Jake Gough <super.jake@hotmail.co.uk>
2018-12-12 16:11:00 +0000

    Update app.ts
    Changed theme order so blue if first.

--- ClientApp/components/app/app.ts
+++ ClientApp/components/app/app.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
+import { SelectedDate } from '../../models/selecteddate';
 import { getCookie, setCookie } from 'tiny-cookie';
 
 @Component({
@@ -10,53 +11,54 @@ import { getCookie, setCookie } from 'tiny-cookie';
 export default class AppComponent extends Vue {
 	drawer: boolean = false;
 	dark: boolean = false;
-	colours: string[] = ["red", "pink", "purple", "indigo", "blue", "teal", "green", "orange"];
+	colours: string[] = ["blue", "teal", "green", "orange", "red", "pink", "purple", "indigo"];
+	selecteddate: SelectedDate = new SelectedDate();
 
 	mounted() {
-		this.themeColour(Number(getCookie('twa-colour')));
+		this.themeColour(Number(getCookie('ers-colour')));
 		this.$vuetify.theme.error = "AF0E14";
-		this.dark = getCookie('twa-dark') == 'true' ? true : false;
+		this.dark = getCookie('ers-dark') == 'true' ? true : false;
 	}
 
 	themeDark() {
 		this.dark = !this.dark;
-		setCookie('twa-dark', this.dark.toString());
+		setCookie('ers-dark', this.dark.toString());
 	}
 
 	themeColour(colour: number) {
-		setCookie('twa-colour', colour.toString());
+		setCookie('ers-colour', colour.toString());
 		switch (colour) {
 			case 0:
-				this.$vuetify.theme.primary = "ED1C24";
-				this.$vuetify.theme.accent = "F36368";
-				break;
-			case 1:
-				this.$vuetify.theme.primary = "E91E63";
-				this.$vuetify.theme.accent = "FF80AB";
-				break;
-			case 2:
-				this.$vuetify.theme.primary = "9C27B0";
-				this.$vuetify.theme.accent = "EA80FC";
-				break;
-			case 3:
-				this.$vuetify.theme.primary = "3F51B5";
-				this.$vuetify.theme.accent = "8C9EFF";
-				break;
-			case 4:
 				this.$vuetify.theme.primary = "2196F3";
 				this.$vuetify.theme.accent = "82B1FF";
 				break;
-			case 5:
+			case 1:
 				this.$vuetify.theme.primary = "009688";
 				this.$vuetify.theme.accent = "A7FFEB";
 				break;
-			case 6:
+			case 2:
 				this.$vuetify.theme.primary = "4CAF50";
 				this.$vuetify.theme.accent = "B9F6CA";
 				break;
-			case 7:
+			case 3:
 				this.$vuetify.theme.primary = "FF9800";
 				this.$vuetify.theme.accent = "FFD180";
+				break;
+			case 4:
+				this.$vuetify.theme.primary = "F44336";
+				this.$vuetify.theme.accent = "FF8A80";
+				break;
+			case 5:
+				this.$vuetify.theme.primary = "E91E63";
+				this.$vuetify.theme.accent = "FF80AB";
+				break;
+			case 6:
+				this.$vuetify.theme.primary = "9C27B0";
+				this.$vuetify.theme.accent = "EA80FC";
+				break;
+			case 7:
+				this.$vuetify.theme.primary = "3F51B5";
+				this.$vuetify.theme.accent = "8C9EFF";
 				break;
 		}
 	}
